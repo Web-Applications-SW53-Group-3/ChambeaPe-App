bfb63cf49634509843fdde0d89d4c564255b20d3
Diego Castro <diego_castro_soto@hotmail.com>
2023-09-27 01:41:01 -0500

    feat: add employer 'my posts' component

---
+++ src/environments/environment.js
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    baseUrl: 'https://65110b963ce5d181df5da5df.mockapi.io/api/chambeape'
+}
--- src/services/login.service.js
+++ src/services/login.service.js
@@ -1,21 +1,22 @@
 import axios from "axios";
+import {environment} from "@/environments/environment";
 
 class LoginService {
     baseUrl= "http://localhost:3000"
     getAll() {
-        return axios.get(this.baseUrl+"/users");
+        return axios.get(environment.baseUrl+"/users");
     }
 
     getByid(id) {
-        return axios.get(this.baseUrl+"/users" + id);
+        return axios.get(environment.baseUrl+"/users" + id);
     }
 
     create(body) {
-        return axios.post(this.baseUrl+"/users", body);
+        return axios.post(environment.baseUrl+"/users", body);
     }
 
     update(id, body) {
-        return axios.put(this.baseUrl+"/users" + id, body);
+        return axios.put(environment.baseUrl+"/users" + id, body);
     }
 }
 
