f802154a56cd3834e16a0f563268035b45bcbc31
Diego Esquivel <U202116749@upc.edu.pe>
2023-09-27 21:21:13 -0500

    feat: add language-selector

---
+++ src/components/language/language-selector.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  data() {
+    return {
+      currentLanguage: 'es',
+    };
+  },
+  methods: {
+    changeLanguage(language) {
+      this.currentLanguage = language;
+
+      this.$i18n.locale = language;
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="language-selector">
+    <button @click="changeLanguage('en')" :class="{ active: currentLanguage === 'en' }">English</button>
+    <button @click="changeLanguage('es')" :class="{ active: currentLanguage === 'es' }">Español</button>
+  </div>
+</template>
+
+<style scoped>
+.language-selector {
+  display: flex;
+}
+
+.language-selector button {
+  background: none;
+  border: none;
+  cursor: pointer;
+  margin-right: 10px;
+  color: #007bff;
+}
+
+.language-selector button.active {
+  font-weight: bold;
+}
+</style>
